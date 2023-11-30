7d83e067076d0e6f969016c0648e2dc595603c43
Lucas Ribeiro <lucasribeiro@MacBook-Air-de-Lucas.local>
2023-11-30 13:34:57 -0300

    resolucao das atividades 2 e 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListItemsComponent } from './components/list-items/list-items.component';
@@ -11,6 +11,7 @@ import { ListItemsComponent } from './components/list-items/list-items.component
     ListItemsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/components/list-items/list-items.component.ts
+++ src/app/components/list-items/list-items.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 
+export interface Item {
+  name: string;
+  type: string;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-list-items',
   templateUrl: './list-items.component.html',
   styleUrls: ['./list-items.component.css']
 })
+
 export class ListItemsComponent {
 
-  itens = [
-    {name: "item1" }] 
+  novoItem: string = '';
+  novoTipo: string = '';
+
+  adicionarTarefa() {
+    if (this.novoItem.trim() !== '' && this.novoTipo.trim() !== '') {
+      // Adicione uma nova tarefa à lista
+      this.itens.push({ name: this.novoItem, type: this.novoTipo, checked: false });
+
+      // Limpe os inputs após adicionar a tarefa
+      this.novoItem = '';
+      this.novoTipo = '';
+    }
+  }
+  removerItem(item: Item) {
+    const index = this.itens.indexOf(item);
+    if (index !== -1) {
+      this.itens.splice(index, 1);
+    }
+  }
+
+  itens: Item[] = [
+    {name: 'arrumar a cama', type: 'tarefas', checked: false },
+    {name: 'lavar a roupa', type: 'tarefas', checked: false },
+    {name: 'fazer o amoço', type: 'tarefas', checked: false},
+    {name: 'jogar bola', type: 'lazer', checked: false}
+  ] 
+
+  marcarItem(item: Item) {
+    item.checked = !item.checked;
+  }
 }
